1b2e1ab3c08bf35dd8b279a3d0048ba21953ba5a
Kethan Ayatti <kethanayatti333@gmail.com>
2025-06-10 18:32:53 +0530

    s

--- votercheck.java
+++ votercheck.java
@@ -1,6 +1,6 @@
 public class votercheck {
     public static void main(String[] args) {
-        int age = Integer.parseInt(args[0]);
+        int age = 20;
         if (age >= 18) {
             System.out.println("You are eligible to vote.");
         } else {
